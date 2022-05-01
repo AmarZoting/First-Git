63b50ccd86ed2bc55bc312c9e28231459372b41d
Amar Zoting <amar.zoting@gmail.com>
2022-05-01 20:05:40 +0530

    added StringDemo.java

---
+++ StringDemo.java
@@ -0,0 +1,22 @@
+package com.my.example;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+
+		String str = "Exampledhfjkhhfd";
+		String revStr = new StringBuilder("Amar").reverse().toString();		
+		char [] strArr = str.toCharArray();
+		char [] newArr = null;
+		System.out.println(revStr);
+		/*
+		 * for (int i = 0; i< str.toCharArray().length; i++) {
+		 * 
+		 * //if(strArr[i] )
+		 * 
+		 * }
+		 */
+		
+	}
+
+}
